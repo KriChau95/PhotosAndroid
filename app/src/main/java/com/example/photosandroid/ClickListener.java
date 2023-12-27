7b9156ec24ec17cb15eb8f86a6dded2b541ac5fa
Krishaan Chaudhary <140979138+KriChau95@users.noreply.github.com>
2023-12-27 13:12:47 -0500

    Update ClickListener.java

--- app/src/main/java/com/example/photosandroid/ClickListener.java
+++ app/src/main/java/com/example/photosandroid/ClickListener.java
@@ -1,9 +1,14 @@
 package com.example.photosandroid;
 
+/**
+ * Interface definition for a callback to be invoked when an item in a RecyclerView is clicked.
+ */
 public interface ClickListener {
-
-
+        /**
+         * Called when an item in a RecyclerView is clicked.
+         *
+         * @param index The index of the clicked item.
+         */
         public void click(int index);
-
 
 }
