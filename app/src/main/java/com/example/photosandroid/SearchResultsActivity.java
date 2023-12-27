c4f039c0ec03c7a2fef85c1a6a17e2211681d9da
Krishaan Chaudhary <140979138+KriChau95@users.noreply.github.com>
2023-12-27 13:15:00 -0500

    Update SearchResultsActivity.java

--- app/src/main/java/com/example/photosandroid/SearchResultsActivity.java
+++ app/src/main/java/com/example/photosandroid/SearchResultsActivity.java
@@ -2,55 +2,45 @@ package com.example.photosandroid;
 
 import static com.example.photosandroid.R.id.*;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-
-
 import java.io.File;
-import java.util.ArrayList;
 
+/**
+ * The {@code SearchResultsActivity} class is an activity class for displaying the search results based on tag.
+ * <p>
+ * @author Krishaan Chaudhary & Joshua Clayton
+ */
 public class SearchResultsActivity extends AppCompatActivity {
     private Album resultsAlbum;
     private ImageAdaptor adaptor;
     private  UserData userData;
 
-
+    /**
+     * Called when the activity is first created. Initializes the UI and sets up event listeners.
+     *
+     * @param savedInstanceState The saved state of the activity.
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_results);
 
-
         //read in the current album
         userData= UserData.getUserdata(getApplicationContext());
-
 
         resultsAlbum = (Album) getIntent().getSerializableExtra("resultsAlbum");
 
         //set add new photo action
         findViewById(R.id.BackButton).setOnClickListener(view -> back());
-
-        //set recycler views data to the photo array
 
         adaptor = new ImageAdaptor(resultsAlbum.getPhotoList(),new ClickListener(){
 
@@ -61,8 +51,8 @@ public class SearchResultsActivity extends AppCompatActivity {
             }
         });
 
+        //set recycler views data to the photo array
         RecyclerView recyclerView = findViewById(resultsList);
-
 
         recyclerView.setAdapter(adaptor);
         recyclerView.setLayoutManager(new LinearLayoutManager(SearchResultsActivity.this));
@@ -70,6 +60,13 @@ public class SearchResultsActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * Handle the result of an activity for getting a new photo.
+     *
+     * @param requestCode The request code.
+     * @param resultCode  The result code.
+     * @param data        The intent data.
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -87,13 +84,21 @@ public class SearchResultsActivity extends AppCompatActivity {
         }
     }
 
-
+    /**
+     * Navigate back to the SearchActivity.
+     */
     public void back(){
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
     }
 
-
+    /**
+     * Get the real path of a URI.
+     *
+     * @param contentURI The URI of the content.
+     * @param context    The activity context.
+     * @return The real path of the content URI.
+     */
     public String getRealPathFromURI(Uri contentURI, Activity context) {
         String[] projection = { MediaStore.Images.Media.DATA };
         @SuppressWarnings("deprecation")
